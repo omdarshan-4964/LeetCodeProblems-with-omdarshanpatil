f025f1a8728c1957f27b1cc5082ef7980dbb4f29
Omdarshan Shindepatil <omdarshanpatil@gmail.com>
2025-03-29 23:24:53 +0530

    LC9 Palindrome

---
+++ Day3_LC9_Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Day3_LC9_Palindrome {
+    public static boolean isPalindrome(int x) {
+
+        if( x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while( x > 0) {
+           int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10; 
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.println(isPalindrome(x));
+
+        sc.close();
+    }
+}
